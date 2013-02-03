c493a31b8ebb55d0d98b52a05bbcfe1de2b174b2
Ps0ke <ps0ke.mail@gmail.com>
2013-02-03 01:46:52 +0100

    Rewrote javascript, faving now works correctly.

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 		$('#search').focus();
 	}
 
+
 	// activate tooltips
 	$('[rel=tooltip]').tooltip();
 
@@ -27,54 +28,135 @@ $(document).ready(function(){
 	});
 	
 
-	// .fav star icon toggle
-	$('.icon-star').mouseenter(function(){
-		$(this).removeClass('icon-star').addClass('icon-star-empty');
+	// star hover toggle
+	$('.fav[class*=icon-star]').mouseenter(function(e){
+		if($(this).hasClass('icon-star')){
+			$(this).removeClass('icon-star').addClass('icon-star-empty');
+		}
+		else if($(this).hasClass('icon-star-empty')){
+			$(this).addClass('icon-star').removeClass('icon-star-empty');
+		}
 	});
-	$('.icon-star').mouseleave(function(){
-		$(this).addClass('icon-star').removeClass('icon-star-empty');
-	});
-	$('.icon-star-empty').mouseleave(function(){
-		$(this).removeClass('icon-star').addClass('icon-star-empty');
-	});
-	$('.icon-star-empty').mouseenter(function(){
-		$(this).addClass('icon-star').removeClass('icon-star-empty');
+
+	$('.fav[class*=icon-star]').mouseleave(function(e){
+		if($(this).hasClass('changed')){
+			$(this).removeClass('changed');
+		}
+		else{
+			if($(this).hasClass('icon-star')){
+				$(this).removeClass('icon-star').addClass('icon-star-empty');
+			}
+			else if($(this).hasClass('icon-star-empty')){
+				$(this).addClass('icon-star').removeClass('icon-star-empty');
+			}
+		}
 	});
 
 
-	// .fav ajax
-	$(".fav").on("click", function(event){
-		var tis = $(this);
+	// Faving shows
+	var faving = function(action, show, successCallback, errorCallback){
 		$.ajax({
-			'url': '/show/' + tis.attr('show'),
+			'url': '/show/' + show,
 			'dataType': 'text',
 			'async': true,
-			'success': function(response){
-				console.log('success! ' + response);
-				if (tis.hasClass('icon-star')){
-					tis.removeClass('icon-star');
-					tis.addClass('icon-star-empty');
-				} else {
-					tis.addClass('icon-star');
-					tis.removeClass('icon-star-empty');
-				}
-				if(tis.parent().get(0).tagName == 'LI'){
-				//	tis.parent().fadeOut(500, function(){ tis.parent().remove() });
-					tis.parent().animate(
-						{ opacity:0, height:0 },
-						500,
-						function(){
-							tis.parent().remove();
-						}
-					);
-				}
-			},
-			'error': function(response){
-				console.log('error! ' + response);
-				// display helpful tooltip
-			 },
-			'type': tis.hasClass('icon-star-empty') ? 'DELETE' : 'PUT'
+			'success': successCallback,
+			'error': errorCallback,
+			'type': action
 		});
+	}
+
+	$('li.search .fav[class*=icon-star]').on('click', function(e){
+		tis = $(this);
+		if(tis.hasClass('icon-star-empty')){
+			faving(
+				'DELETE',
+				tis.attr('show'),
+				function(response){
+					console.log('sucess');
+					tis.addClass('changed');
+					tis.tooltip('destroy');
+					tis.attr('title', 'Add to favorites');
+					tis.tooltip();
+				},
+				function(resonse){
+					console.log('error');
+				}
+			);
+		}
+		else if(tis.hasClass('icon-star')){
+			faving(
+				'PUT',
+				tis.attr('show'),
+				function(response){
+					console.log('sucess');
+					tis.addClass('changed');
+					tis.tooltip('destroy');
+					tis.attr('title', 'Delete from favorites');
+					tis.tooltip();
+				},
+				function(resonse){
+					console.log('error');
+				}
+			);
+		}
+		return false;
+	});
+
+	$('li.home i.fav.icon-star').on('click', function(e){
+		var tis = $(this);
+		faving(
+			'DELETE',
+			tis.attr('show'),
+			function(response){
+				console.log('sucess');
+				tis.parent().animate(
+					{ opacity:0, height:0 },
+					500,
+					function(){
+						tis.parent().remove();
+					}
+				);
+			},
+			function(resonse){
+				console.log('error');
+			}
+		);
+		return false;
+	});
+
+	$('button.fav').on('click', function(e){
+		var tis = $(this);
+		if(tis.hasClass('add')){
+			faving(
+				'PUT',
+				tis.attr('show'),
+				function(response){
+					console.log('sucess');
+					tis.addClass('remove');
+					tis.removeClass('add');
+					tis.text('Remove from favorites');
+				},
+				function(resonse){
+					console.log('error aaaaaaadd');
+				}
+			);
+		}
+		else if(tis.hasClass('remove')){
+			faving(
+				'DELETE',
+				tis.attr('show'),
+				function(response){
+					console.log('sucess');
+					tis.addClass('add');
+					tis.removeClass('remove');
+					tis.text('Add to favorites');
+					$('input[type=checkbox]').removeAttr('checked');
+				},
+				function(resonse){
+					console.log('error remooooooovee');
+				}
+			);
+		}
 		return false;
 	});
 
@@ -82,8 +164,7 @@ $(document).ready(function(){
 	// watch ajax
 	$("input[type=checkbox].episode").on("change", function(event){
 		var tis = $(this);
-		var season = $(this).attr('id').split('_')[0];
-		var episode = $(this).attr('id').split('_')[1];
+		var [season, episode] = tis.attr('id').split('_');
 		var show = location.pathname.split('/').pop();
 		$.ajax({
 			'url': '/show/' + show + '/' + season + '/' + episode,
@@ -94,15 +175,30 @@ $(document).ready(function(){
 			},
 			'error': function(response){
 				console.log('error! ' + response);
-				// display helpful tooltip
-				if (tis.attr('checked') == 'checked'){
-					tis.removeAttr('checked');
-				} else {
-					tis.attr('checked', 'checked');
-				}
+				/* Only reasonable if auto-adding will be removed from server.
+				faving(
+					'PUT',
+					show,
+					function(res){
+						console.log('success');
+						var btn = $('#favbtn');
+						btn.addClass('remove');
+						btn.removeClass('add');
+						btn.text('Remove from favorites');
+					},
+					function(res){
+						if (tis.attr('checked') == 'checked'){
+							tis.removeAttr('checked');
+						} else {
+							tis.attr('checked', 'checked');
+						}
+					}
+				);
+				*/
 			 },
 			'type': tis.attr('checked') == 'checked' ? 'PUT' : 'DELETE'
 		});
 
 	});
 });
+
